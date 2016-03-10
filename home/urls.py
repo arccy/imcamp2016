69e1b4b3e1870c1f08405902f0d811b0793686ab
Sean Liao <seanliao96@gmail.com>
2016-03-10 21:38:12 +0800

    nginx preparations

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^404', views.error404, name='404'),
-    url(r'^$', views.home, name='home'),
-]
--- home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def home(request):
-    return render(request, 'home/index.html')
-
-def error404(request):
-    return render(request, 'home/404.html', status=404)
