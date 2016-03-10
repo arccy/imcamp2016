e6c1ca28f349125f30a862b42da81f0e16b69a02
Sean Liao <seanliao96@gmail.com>
2016-03-10 22:03:54 +0800

    url changes

--- imcamp/settings.py
+++ imcamp/settings.py
@@ -119,5 +119,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/camp/2016/static/'
 STATIC_ROOT = 'C:/Users/seanl/Dropbox/imcamp/static/'
