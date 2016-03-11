8ca2a3987b601b8295da74e785c28d03a998fb53
Sean Liao <seanliao96@gmail.com>
2016-03-12 02:53:23 +0800

    routing update

--- signup/views.py
+++ signup/views.py
@@ -1,3 +1,4 @@
+from django.template import RequestContext
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
@@ -8,6 +9,7 @@ from .models import SignUpForm
 def sign(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST, request.FILES)
+        context_instance=RequestContext(request)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/camp/2016/signup/thanks/')
@@ -16,6 +18,7 @@ def sign(request):
         
     else:
         form = SignUpForm()
+        context_instance=RequestContext(request)
     return render(request, 'signup/signup.html', {'form': form})
 
 def thanks(request):
