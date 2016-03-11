fa3e53638fa0d1f166b0a2b9dbd948c163409a01
Sean Liao <seanliao96@gmail.com>
2016-03-11 19:06:21 +0800

    urls

--- signup/urls.py
+++ signup/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^thanks/', views.thanks, name='thanks'),
+    url(r'^thanks', views.thanks, name='thanks'),
     url(r'^$', views.sign, name='sign'),
+    url(r'', views.sign, name='sign'),
 ]
