b68f2f80a7a61503b8551a149524a45e005f4c8e
Sean Liao <seanliao96@gmail.com>
2016-04-30 23:52:46 +0800

    tshirt size chart

--- signup/urls.py
+++ signup/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^thanks/', views.thanks, name='thanks'),
     url(r'thanks', views.thanks, name='thanks'),
-    url(r'^size/', views.thanks, name='sizes'),
-    url(r'size', views.thanks, name='sizes'),
+    url(r'^size/', views.size, name='size'),
+    url(r'size', views.size, name='size'),
     url(r'^$', views.sign, name='sign'),
     url(r'', views.sign, name='sign'),
 ]
--- signup/views.py
+++ signup/views.py
@@ -23,6 +23,5 @@ def sign(request):
 def thanks(request):
     return render(request, 'signup/thanks.html')
 
-def sizes(request):
-    image_data = open('/camp/2016/static/signup/sizes.jpg','rb').read()
-    return HttpResponse(image_data, conetent="image/jpg")
+def size(request):
+    return render(request, 'signup/size.html')
